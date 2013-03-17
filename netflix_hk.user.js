bcfd925d8351d545d3818fd676e8bb15e067416c
Keith Batten <kbatten@gmail.com>
2013-03-16 18:37:00 -0700

    jslint

--- netflix_hk.user.js
+++ netflix_hk.user.js
@@ -2,7 +2,7 @@
 // @name           Netflix Hotkeys
 // @description    Adds hotkeys to netflix.com
 // @match          *://*.netflix.com/*
-// @version        1
+// @version        2
 // ==/UserScript==
 
 // w - previous row
@@ -11,39 +11,43 @@
 // d - scroll right
 // f - start video
 
+/*jslint browser: true*/
+/*jslint vars: true */
+
 var row = 0;
 var videoIndex = 0;
 
 function clearItem(row, videoIndex) {
+    "use strict";
     try {
         var elem = document.getElementById("slider_" + row).childNodes[1].childNodes[videoIndex];
-        elem.style.border="none";
-    }
-    catch (e) {
+        elem.style.border = "none";
+    } catch (err) {
     }
 }
 function selectItem(row, videoIndex) {
+    "use strict";
     try {
         var elem = document.getElementById("slider_" + row).childNodes[1].childNodes[videoIndex];
-        elem.style.border="thin dashed #CC0000";
+        elem.style.border = "thin dashed #CC0000";
         elem.scrollIntoView();
         window.scrollBy(0, -100);
-    }
-    catch (e) {
+    } catch (err) {
     }
 }
 function clickItem(row, videoIndex) {
+    "use strict";
     try {
         var elem = document.getElementById("slider_" + row).childNodes[1].childNodes[videoIndex].childNodes[1].childNodes[1];
-        var e = document.createEvent("MouseEvents");
-        e.initEvent("click", true, true);
-        elem.dispatchEvent(e);
-    }
-    catch (e) {
+        var ev = document.createEvent("MouseEvents");
+        ev.initEvent("click", true, true);
+        elem.dispatchEvent(ev);
+    } catch (err) {
     }
 }
 
 function scrollHorizontal(direction) {
+    "use strict";
     direction = direction / Math.abs(direction);
 
   // clear previously selected item
@@ -59,20 +63,18 @@ function scrollHorizontal(direction) {
         } else {
             scrollHorizontal(direction);
         }
-    }
-    catch (e) {
+    } catch (err) {
         if (videoIndex < 0) {
             // scroll right till we hit something
             scrollHorizontal(1);
-            return;
         } else {
             // scroll left till we hit something
-            scrollHorizontal(-1)
-            return;
+            scrollHorizontal(-1);
         }
     }
 }
 function scrollVertical(offset) {
+    "use strict";
     clearItem(row, videoIndex);
 
     row += offset;
@@ -85,8 +87,9 @@ function scrollVertical(offset) {
     scrollHorizontal(1);
 }
 
-document.onkeydown = function (e) {
-    switch (e.keyCode) {
+document.onkeydown = function (ev) {
+    "use strict";
+    switch (ev.keyCode) {
     case 65: // "A"
         scrollHorizontal(-1);
         break;
@@ -101,10 +104,11 @@ document.onkeydown = function (e) {
         break;
     case 70: // "F"
         clickItem(row, videoIndex);
+        break;
     default:
         break;
     }
-}
+};
 
 // init to first row, first index available
 scrollVertical(0);
